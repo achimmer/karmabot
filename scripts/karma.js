b005c1ea34390d60a2bbdec052be1ab40e49d4b3
Andrew Himmer <achimmer@gmail.com>
2016-07-10 20:44:36 -0500

    changes

--- scripts/karma.js
+++ scripts/karma.js
@@ -14,8 +14,8 @@
 
 module.exports = function(robot) {
 
-  robot.respond(/@name +1/i, addPoints);
-  robot.respond(/@name -1/i, subPoints);
+  robot.respond(/achimmer +1/i, addPoints);
+  robot.respond(/achimmer -1/i, subPoints);
   robot.respond(/points/i, printPointCount);
 
   var numPoints = 0;
@@ -26,6 +26,8 @@ module.exports = function(robot) {
    */
   function addPoints(msg) {
     var message,
-        addPointCount = parseInt(msg.match[1], 10); // converts the string '10' to the number 10. This number is what was said to hubot in the slack channel  
+        addPointCount = parseInt(msg.match[1], 10); // converts the string '10' to the number 10. This number is what was said to hubot in the slack channel
+  message = 'achimmers karma has increased to 10 points';
+  msg.send(message);
   }
 };
