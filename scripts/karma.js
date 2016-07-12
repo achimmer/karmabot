e3495853292d2c3f907f2b89d22d9373311bc715
Andrew Himmer <achimmer@gmail.com>
2016-07-11 21:17:41 -0500

    Testing message.

--- scripts/karma.js
+++ scripts/karma.js
@@ -14,28 +14,26 @@
 
 module.exports = function(robot) {
 
-  robot.respond(/@[a-z]+/gi, addPoints);
-  robot.respond(/@[a-z]+ -1/gi, subPoints);
+  robot.respond(/@[a-z]+/gi, parseMessage); // rule
+  // robot.respond(/@[a-z]+ -1/gi, subPoints);
   // robot.respond(/points/gi, printPointCount);
 
   // var numPoints = 0;
   var points = {};
 
-  function addPoints(msg) {
+  function parseMessage(msg) {
+    console.log('msg', msg);
 
+    // pull out userName
+    var matches = msg.match(/@([a-z]+)/gi);
+    console.log('matches', matches);
+
+    // find out how many points
+    // find out which direction
     if(!points[userName]){
       points[userName] = 0;
     }
     points[userName] += numPoints;
-    var message = userName + 'karma has increased to' + numPoints + 'points';
-    msg.send(message);
-  }
-
-  function subPoints(userName, numPoints, msg) {
-    if(!points[userName]){
-      points[userName] = 0;
-    }
-    points[userName] -= numPoints;
     var message = userName + 'karma has increased to' + numPoints + 'points';
     msg.send(message);
   }
