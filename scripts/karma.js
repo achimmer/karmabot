2b8091e53c58a4b388d387d86ab301055ab73e7e
Andrew Himmer <achimmer@gmail.com>
2016-07-11 21:32:23 -0500

    More debugging

--- scripts/karma.js
+++ scripts/karma.js
@@ -22,7 +22,7 @@ module.exports = function(robot) {
   var points = {};
 
   function parseMessage(msg) {
-    console.log('msg', msg.message.rawMessage);
+    console.log('msg', msg.message);
 
     // pull out userName
     // var matches = msg.match(/@([a-z]+)/gi);
